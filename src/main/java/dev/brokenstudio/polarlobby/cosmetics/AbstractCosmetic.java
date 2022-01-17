bd018085ca9e45bdf2643bda62fe321e87053a82
Jan Zager <jan.zager@brokenstudio.de>
2022-01-17 19:57:10 +0100

    Started with cosmetics

---
+++ src/main/java/dev/brokenstudio/polarlobby/cosmetics/AbstractCosmetic.java
@@ -0,0 +1,44 @@
+package dev.brokenstudio.polarlobby.cosmetics;
+
+import dev.brokenstudio.polarlobby.utils.PolarItem;
+import org.bukkit.entity.Player;
+
+public abstract class AbstractCosmetic {
+
+    private final String name;
+    private final String description;
+    private final CosmeticType cosmeticType;
+    private final PolarItem item;
+    private final Rarity rarity;
+
+
+    public AbstractCosmetic(String name, String description, CosmeticType cosmeticType, PolarItem item, Rarity rarity) {
+        this.name = name;
+        this.description = description;
+        this.cosmeticType = cosmeticType;
+        this.item = item;
+        this.rarity = rarity;
+        this.item.name(this.rarity.color + this.name);
+        this.item.lore("§7Kategorie§8: §f" + this.cosmeticType.name,"§7Seltenheit§8: " + this.rarity.displayname()," ", "§7" + this.description, " ");
+    }
+
+    public abstract void select(Player player);
+    public abstract void use(Player player);
+    public abstract void deselect(Player player);
+
+    public String getName() {
+        return name;
+    }
+
+    public CosmeticType getCosmeticType() {
+        return cosmeticType;
+    }
+
+    public PolarItem getItem() {
+        return item;
+    }
+
+    public Rarity getRarity() {
+        return rarity;
+    }
+}
---
+++ src/main/java/dev/brokenstudio/polarlobby/cosmetics/CosmeticType.java
@@ -0,0 +1,12 @@
+package dev.brokenstudio.polarlobby.cosmetics;
+
+public enum CosmeticType {
+
+    HAT("Hut"),BANNER("Banner");
+
+    String name;
+
+    CosmeticType(String name) {
+        this.name = name;
+    }
+}
